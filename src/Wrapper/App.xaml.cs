2fdc57c9152fa4fcd7566ca3193e65d0709a6954
Siarhei Kuchuk <siarheik@outlook.com>
2022-02-22 02:25:56 +0300

    in progress

--- src/Wrapper/App.xaml.cs
+++ src/Wrapper/App.xaml.cs
@@ -1,46 +1,30 @@
 ﻿using Microsoft.UI.Xaml;
+using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Core;
+using Windows.Foundation.Metadata;
 using Windows.Globalization;
 using Windows.Storage;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace AudioRecorderV4
+namespace Wrapper
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            Package package = Package.Current;
-            PackageId packageId = package.Id;
-            PackageVersion version = packageId.Version;
-
-            var appFolder = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var binFolder = System.IO.Path.Combine(appFolder, "bin");
-
-            Process.Start(new ProcessStartInfo() { CreateNoWindow=true, UseShellExecute=true, FileName= "cmd", Arguments = $"/c \"start /b gimp-{version.Major}.{version.Minor}.exe\"",  WorkingDirectory = binFolder });
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync().AsTask().Wait(); ;
+            }
         }
     }
 }
